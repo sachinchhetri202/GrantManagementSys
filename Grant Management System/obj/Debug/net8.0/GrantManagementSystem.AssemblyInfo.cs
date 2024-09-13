2669a4ea2d4b4ee9b80d6ec22240bf7e29d4b130
Sachin Chhetri <spaudelchhetri@mail.weber.edu>
2024-09-13 17:38:23 -0600

    .

--- Grant Management System/obj/Debug/net8.0/GrantManagementSystem.AssemblyInfo.cs
+++ Grant Management System/obj/Debug/net8.0/GrantManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrantManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d7ce350ea86f7678604d768bbf64fb35e21a9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7d1d98fb20268d3637193f72c77bc9d8c263477")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrantManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrantManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
