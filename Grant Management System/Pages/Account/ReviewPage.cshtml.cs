1b6f5f137783aa88ee4582ac0d4b63ec20c59615
Sachin Chhetri <spaudelchhetri@mail.weber.edu>
2024-09-16 18:43:36 -0600

    Added Review Page

---
+++ Grant Management System/Pages/Account/ReviewPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Grant_Management_System.Pages.Account
+{
+    public class ReviewPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Grant Management System/obj/Debug/net8.0/GrantManagementSystem.AssemblyInfo.cs
+++ Grant Management System/obj/Debug/net8.0/GrantManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrantManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+321c85aad4b18d14f6e28d1b5e17406d6475d044")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8469118d37df021991106d7d35519e9b26c0846")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrantManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrantManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
